9bd577d3519aa7d3e3c82dce482cbf5274cc8243
eXo-Necro <16350302+eXo-Necro@users.noreply.github.com>
2022-01-27 17:12:29 +0100

    Update and rename youtube.css to youtube.js

--- youtube.js
+++ youtube.js
@@ -1,5 +1,13 @@
+(function() {
+    'use strict';
+ 
+    var style = `
 body.efyt-wide-player.viewport:not(.efyt-mini-player) ytd-watch-flexy[theater] #movie_player:not(.ytp-fullscreen) video.html5-main-video, body.efyt-wide-player.viewport:not(.efyt-mini-player) ytd-watch-flexy[theater] #movie_player:not(.ytp-fullscreen) .ytp-iv-video-content {
   width: calc(100vw - 17px) !important;
   left: 0px !important;
   top: 0px !important;
-}
+}`;
+    var elem = document.createElement('style');
+    elem.innerText = style;
+    document.head.appendChild(elem);
+})();
